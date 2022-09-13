e00a46bf5fdc7597bdac3ac8ec422015019330a3
Your Namecls <erencan_gvrcn@outlook.com>
2022-09-13 10:49:11 +0300

    Proje dosyası ekle.

---
+++ CA_Employee/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_Employee
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+
+
+
+        public void Create(Employee employee)
+        {
+            Console.WriteLine("Lütfen çalışan adı girin");
+
+            employee.EmployeeFirstName = Console.ReadLine();
+            Validation(employee.EmployeeFirstName);
+            Console.WriteLine("Lütfen çalışanın soyadını girin");
+            employee.EmployeeLastName = Console.ReadLine();
+            Validation(employee.EmployeeLastName);
+            Console.WriteLine("Çalışan oluşturuldu");
+        }
+
+        void Validation(string deger)
+        {
+            if(string.IsNullOrEmpty(deger))
+            {
+                Console.WriteLine("Değer boş bırakılamaz");
+                Environment.Exit(0);
+
+            }
+        }
+        public void Printing(Employee employee)
+        {
+            Console.WriteLine($"Çalışanın ismi:{employee.EmployeeFirstName}\nÇalışanın soyismi: {employee.EmployeeLastName}");
+        }
+    }
+}
---
+++ CA_Employee/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CA_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Karşilama();
+            Employee employee = new Employee();
+            employee.Create(employee);
+            employee.Printing(employee);
+            Console.Read();
+
+
+
+            void Karşilama()
+            {
+                Console.WriteLine("Hoşgeldiniz");
+            }
+        }
+    }
+}
